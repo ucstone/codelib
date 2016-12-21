8ab11df7f9a2a7c64a66a51b0082e206889a3d0c
ucstone <jc_16@foxmail.com>
2016-12-21 11:49:04 +0800

    Update ZoomEyeV2.0.py

--- ZoomEyeV2.0.py
+++ ZoomEyeV2.0.py
@@ -14,7 +14,9 @@ class Zoomeye(object):
         self._headers = {"Authorization": "JWT %s" % self.get_token(), "Content-Type": "application/json"}
 
     def get_token(self):
-        payload = {"username": "你的ZoomEye账号", "password": "你的ZoomEye密码"}
+        payload = {"username": "595126297@qq.com", "password": "5063785@sha"}
+        # payload = {"username": "stone@sec.dog", "password": "Ixw3vFVc65Kk"}
+        # payload = {"username": "你的ZoomEye账号", "password": "你的ZoomEye密码"}
         try:
             res = req.post('https://api.zoomeye.org/user/login', data=json.dumps(payload))
         except Exception as e:
@@ -26,30 +28,17 @@ class Zoomeye(object):
     def resource_info(self):
         return req.get("https://api.zoomeye.org/resources-info", headers=self._headers).content
 
-    def search(self, keyword, page=1, searchtype="web"):
-        uri = 'https://api.zoomeye.org/%s/search?query=%s&page=%s&fact=app,os' % (searchtype, urllib.quote(keyword), page)
-        try:
-            response = req.get(uri, headers=self._headers)
-            search_res = json.loads(response.content)
-            print response.url
-        except Exception as e:
-            print e
-        # 此处判断有无搜索到结果，如果响应代码为200表示请求到结果，否则打印错误提示
-        if response.status_code == 200:
-            pages = self.getPageNum(int(search_res['total']))
-        else:
-            print "Error Code: ",response.status_code, "Tips: ",search_res
-            sys.exit()
-        #执行查询操作
-        for i in range(page,pages+1):
+    # 获取查询内容
+    def get_content(self, searchtype, keyword, startpage, endpage):
+        for i in xrange(startpage,endpage+1):
             print "Get page " + str(i) + " info ..."
-            uri = 'https://api.zoomeye.org/%s/search?query=%s&page=%s&fact=app,os' % (searchtype, urllib.quote(keyword), page)
+            uri = 'https://api.zoomeye.org/%s/search?query=%s&page=%s&fact=app,os' % (searchtype, urllib.quote(keyword), i)
             try:
                 result_page = req.get(uri, headers=self._headers)
                 page_content = json.loads(result_page.content)
-                print type(page_content)
             except Exception as e:
                 print e
+            print type(page_content)
             if result_page.status_code == 200:
                 # 搜索有两种类型，一种是host，另一种是web服务
                 if searchtype == 'host':
@@ -66,10 +55,16 @@ class Zoomeye(object):
                         print res_line #此行可注释，不影响查询结果
                         self.save_result(str(res_line).encode('utf-8'))
             else:
-                print "Error Code: %s" % result_page.status_code
-                print "URL: {}, Error MSG: {}, Tips:{} ".format(search_res['url'], search_res['message'],
-                                                                search_res['error'])
+                print "Error Code: %s" % result_page.status_code, result_page.content
             sleep(0.2)
+
+    # 执行查询操作
+    def search(self, keyword, page=1, searchtype="web"):
+        uri = 'https://api.zoomeye.org/%s/search?query=%s&page=%s&fact=app,os' % (searchtype, urllib.quote(keyword), page)
+        res_content = []
+        pages = self.getPageNum(uri)
+        #执行查询操作
+        self.get_content(searchtype=searchtype, keyword=keyword,startpage=page, endpage=pages)
 
     # 将结果写入文件
     def save_result(self, targetList):
@@ -77,18 +72,29 @@ class Zoomeye(object):
             f.writelines(targetList+ '\n')
 
     # 对搜索结果进行分页
-    def getPageNum(self, total):
-        if total == 0:
-            print "No result, exit.."
+    def getPageNum(self, uri):
+        try:
+            response = req.get(uri, headers=self._headers)
+            search_res = json.loads(response.content)
+            print response.url
+        except Exception as e:
+            print e
+        # 此处判断有无搜索到结果，如果响应代码为200表示请求到结果并返回页数，否则打印错误提示
+        if response.status_code == 200:
+            total = int(search_res['total'])
+            page = total / 10
+            if total % 10 == 0:
+                return page
+            print page
+            return page + 1
+        else:
+            print "Error Code: ",response.status_code, "Tips: ",search_res
             sys.exit()
-        page = total / 10
-        if total % 10 == 0:
-            return page
-        return page + 1
 
 if __name__ == "__main__":
     z = Zoomeye()
     # 查看当月还有多少剩余查询结果
     print z.resource_info()
     # 查询
-    print z.search("openssh", searchtype="host",page=1)
+    # print z.search("city:shijiazhuang webapp:dd", searchtype="host",page=1)
+    print z.search(keyword="city:shijiazhuang openssh", searchtype='host', page=37)
