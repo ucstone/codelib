5e57ebef5f4b1bf780fb5eddd42bc3555b783a74
ucstone <jc_16@foxmail.com>
2016-12-21 10:56:07 +0800

    Update ZoomEyeV2.0.py

--- ZoomEyeV2.0.py
+++ ZoomEyeV2.0.py
@@ -14,7 +14,7 @@ class Zoomeye(object):
         self._headers = {"Authorization": "JWT %s" % self.get_token(), "Content-Type": "application/json"}
 
     def get_token(self):
-        payload = {"username": "你的邮箱", "password": "你的密码"}
+        payload = {"username": "你的ZoomEye账号", "password": "你的ZoomEye密码"}
         try:
             res = req.post('https://api.zoomeye.org/user/login', data=json.dumps(payload))
         except Exception as e:
@@ -26,7 +26,6 @@ class Zoomeye(object):
     def resource_info(self):
         return req.get("https://api.zoomeye.org/resources-info", headers=self._headers).content
 
-
     def search(self, keyword, page=1, searchtype="web"):
         uri = 'https://api.zoomeye.org/%s/search?query=%s&page=%s&fact=app,os' % (searchtype, urllib.quote(keyword), page)
         try:
@@ -35,7 +34,13 @@ class Zoomeye(object):
             print response.url
         except Exception as e:
             print e
-        pages = self.getPageNum(int(search_res['total']))
+        # 此处判断有无搜索到结果，如果响应代码为200表示请求到结果，否则打印错误提示
+        if response.status_code == 200:
+            pages = self.getPageNum(int(search_res['total']))
+        else:
+            print "Error Code: ",response.status_code, "Tips: ",search_res
+            sys.exit()
+        #执行查询操作
         for i in range(page,pages+1):
             print "Get page " + str(i) + " info ..."
             uri = 'https://api.zoomeye.org/%s/search?query=%s&page=%s&fact=app,os' % (searchtype, urllib.quote(keyword), page)
@@ -46,15 +51,19 @@ class Zoomeye(object):
             except Exception as e:
                 print e
             if result_page.status_code == 200:
+                # 搜索有两种类型，一种是host，另一种是web服务
                 if searchtype == 'host':
                     for match in page_content['matches']:
-                        res_line = match['ip'] + ':' + str(match['portinfo']['port'])+'\t'+match['geoinfo']['isp']
+                        # print match
+                        # 整理host搜索结果，这里可以自己看返回包自定义
+                        res_line = match['ip'] + ':' + str(match['portinfo']['port'])+'\t'+match['portinfo']['banner'].strip()+'\t'+match['geoinfo']['isp']
                         print res_line
                         self.save_result(res_line)
                 elif searchtype == 'web':
                     for match in page_content['matches']:
+                        # 整理web搜索结果，这里可以自己看返回包自定义
                         res_line = match['ip'][0] +'\t'+ match['title'] +'\t'+ 'http://' + match['site']
-                        print match['ip'][0]
+                        print res_line #此行可注释，不影响查询结果
                         self.save_result(str(res_line).encode('utf-8'))
             else:
                 print "Error Code: %s" % result_page.status_code
@@ -62,10 +71,12 @@ class Zoomeye(object):
                                                                 search_res['error'])
             sleep(0.2)
 
+    # 将结果写入文件
     def save_result(self, targetList):
         with open('result.txt', 'a') as f:
             f.writelines(targetList+ '\n')
 
+    # 对搜索结果进行分页
     def getPageNum(self, total):
         if total == 0:
             print "No result, exit.."
@@ -80,4 +91,4 @@ if __name__ == "__main__":
     # 查看当月还有多少剩余查询结果
     print z.resource_info()
     # 查询
-    print z.search("city:jinan struts2", searchtype="web")
+    print z.search("openssh", searchtype="host",page=1)
